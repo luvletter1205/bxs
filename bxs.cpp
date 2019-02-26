4aac0da3632c6d30e62595585a2c939c4cfd2ea2
sherrlock0535 <45725982+sherrlock0535@users.noreply.github.com>
2019-02-26 15:08:31 +0800

    Add files via upload

--- bxs.cpp
+++ bxs.cpp
@@ -1,4 +1,3 @@
-//code by sherrlock
 #include<iostream>
 #include<cstdio>
 #include<cstring>
@@ -6,9 +5,11 @@
 #include<map>
 #include<cmath>
 #include<algorithm>
+#include<iomanip>
 #include<fstream>
 #include "windows.h"
-#include "shlobj.h" 
+#include "shlobj.h"
+#include<vector>
 
 #define max_num_of_variables 10000
 #define max_num_of_code_lines 10000
@@ -16,10 +17,16 @@
 using namespace std;
 
 
-double X[max_num_of_variables];int v_id=0; 	 //variable and its id
+long double X[max_num_of_variables];int v_id=0; 	 //variable and its id
 map <string,int> var;						 //RB-tree conect variable and its id
 char str[max_num_of_code_lines][120];		 //Save the code
 int line;									 //line of code
+
+bool isBlack(const char& ch)
+{
+	if(ch==' '||ch=='\n'||ch=='\t')return true;
+	return false;
+}
 
 int vdef (string variable_name)      		 //define a new variable
 {
@@ -27,7 +34,7 @@ int vdef (string variable_name)      		 //define a new variable
 	return v_id;
 }
 
-int vcal (string variable_name)
+long double vcal (string variable_name)
 {
 	if(var.find(variable_name) == var.end() )
 		return -1;
@@ -35,7 +42,7 @@ int vcal (string variable_name)
 		return X[var[variable_name]];
 }
 
-void assignment (string variable_name,double num)	//assignment a variable
+void assignment (string variable_name,long double num)	//assignment a variable
 {
 	
 	if(var.find(variable_name) == var.end() )
@@ -44,14 +51,89 @@ void assignment (string variable_name,double num)	//assignment a variable
 		X[var[variable_name]]=num;
 }
 
-void INPUT_ (string variable_name)
+//apart sentences
+//char b[10][101];
+/*void apart(int line_id)
 {
-	
+	int len=strlen(str[line_id]);
+	cout<<"apart note: "<<len<<endl;
+	int top=1,size=0;
+	bool bl;
+	for(int i=0;i<len;i++)
+	{
+		cout<<"for note: "<<str[line_id][i]<<endl;
+		if(str[line_id][i]==' ')
+		{
+			if(bl==0)
+				continue;
+			if(bl==1)
+			{
+				top++;
+				size=0;
+				bl=0;
+			}
+		}
+		else 
+		{
+			bl=1;
+			b[top][size]=str[line_id][i];
+			size++;
+			cout<<b[top]<<endl;
+		}
+	}
+//	cout<<"apart note: "<<b[1]<<" "<<b[2]<<endl;
+}*/
+
+vector <string> apart(const int &line_id)
+{
+	vector<string> words;
+	string tmp="";
+	for(auto i:str[line_id])
+	{
+		if(isBlack(i)&&tmp!="")
+		{
+			words.push_back(tmp);
+			tmp="";
+		}
+		else tmp+=i;
+	}
+	if(tmp!="")
+		words.push_back(tmp);
+	return words;
 }
 
-void PRINT_ (string variable_name)
+void INPUT_ (vector<string>tmp)
 {
-	
+	if(tmp[1][0]=='\"')
+	{
+		int len=tmp[1].length();
+		for(int i=1;i<len-2;i++)
+			cout<<tmp[1][i];
+		cout<<endl;
+		double x;
+		cin>>x;
+		assignment(tmp[2],x);
+	}
+	else
+	{
+		double x;
+		cin>>x;
+		assignment(tmp[1],x);
+	}
+}
+
+void PRINT_ (vector<string>tmp)
+{
+	if(tmp[1][0]=='\"')
+	{
+		int len=tmp[1].length();
+		for(int i=1;i<len-2;i++)
+			cout<<tmp[1][i];
+		cout<<endl;
+		cout<<vcal(tmp[2]);
+	}
+	else
+		cout<<vcal(tmp[1]);
 }
 
 
@@ -68,29 +150,28 @@ string  getDesktopPath()
         }
         pShellMalloc->Release();
     }
-
     return string(szDir);
 }
 
 
-void solve_sentence(int line_id)
+void solve_sentence(const int &line_id)
 {
-	string strs;
-	char ch;
-	for(int i=0;i++;str[line_id][strs])
-	{
-		ch=getchar();
-		strs[i]=ch;
-	}
-	if(strs=="PRINT")
-		PRINT_();
-	else if 
+	
+	/*if(strs=="PRINT")
+		PRINT_(line_id);
+	else if (strs=="INPUT")
+		INPUT_(line_id);*/
+	auto tmp=apart(line_id);
+	if(tmp[0]=="PRINT")
+		PRINT_(tmp);
+	if(tmp[0]=="INPUT")
+		INPUT_(tmp);
+	
 }
 
-void read()
+void read(string name)
 {
-	string name;
-	cin>>name;
+	//cout<<name<<endl;
 	name+=".bxs";
 	string path = getDesktopPath();
 	path += "\\" ;
@@ -100,10 +181,10 @@ void read()
 	while(in.getline(str[++line],100));
 }
 
-int main()
+int main(int argc,char **argv)
 {
-	
-	read();
-	for(int i=1;i<=line;i++)
-		cout<<str[i]<<endl;
+	cout<<fixed;
+	read(string(argv[1]));
+	solve_sentence(1);
+	solve_sentence(2);
 }
